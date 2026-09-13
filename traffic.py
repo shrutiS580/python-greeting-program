e7154f0a10617598861642192419db73b64df20c
shruti swami <shrutiswami791@gmail.com>
2026-09-13 14:12:09 +0530

    signal

---
+++ traffic.py
@@ -0,0 +1,12 @@
+signal = input("enter signal color:")
+if signal == "red": 
+ print("stop")
+
+elif signal =="yellow" :
+ print("get ready")
+
+elif signal == "green":
+ print("Go")
+
+else:
+ print("invalid signal col")
